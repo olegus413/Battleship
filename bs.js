e021e1b2aa816910db3914bd5069763a8debc1c0
Олег <73531304+olegus413@users.noreply.github.com>
2021-01-10 16:54:20 +0400

    stable_version_1.0.0

--- bs.js
+++ bs.js
@@ -11,14 +11,46 @@ let view = {
         let cell = document.getElementById(location);
         cell.setAttribute('class', 'miss');
     }
-}
+};
 
 let controller = {
     guesses: 0,
 
     processGuess: function (guess) {
-
+        let location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            let hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage('Ты потопил все корабли с ' + this.guesses + ' попытками');
+            }
+        }
     }
+}
+
+window.onload = init;
+
+function init() {
+    let fireButton = document.getElementById('fireButton');
+    fireButton.onclick = handleFireButton;
+    let guessInput = document.getElementById('guessInput');
+    guessInput.onkeypress = handleKeyPress;
+    model.generateShipLocations();
+}
+
+function handleKeyPress(e) {
+    let fireButton = document.getElementById('fireButton');
+    if (e.key === 'Enter') {
+        fireButton.click();
+        return false;
+    }
+}
+
+function handleFireButton() {
+    let guessInput = document.getElementById('guessInput');
+    let guess = guessInput.value;
+    controller.processGuess(guess);
+    guessInput.value = '';
 }
 
 
@@ -34,21 +66,23 @@ function parseGuess(guess) {
             alert('Введите корректные данные');
         } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model) {
             alert('введите корректные данные');
+        } else {
+            return row + column;
         }
-
+        return null;
     }
 }
 
 let model = {
-    boardSize: 9,
+    boardSize: 10,
     numShips: 3,
     shipLength: 3,
     shipsSunk: 0,
 
     ships: [
-        {locations: ['10', '20', '30'], hits: ['', '', '']},
-        {locations: ['32', '33', '34'], hits: ['', '', '']},
-        {locations: ['63', '64', '65'], hits: ['', '', '']},
+        {locations: [0, 0, 0], hits: ['', '', '']},
+        {locations: [0, 0, 0], hits: ['', '', '']},
+        {locations: [0, 0, 0], hits: ['', '', '']},
     ],
 
 
@@ -79,5 +113,49 @@ let model = {
             }
         }
         return true;
+    },
+    generateShipLocations: function () {
+        let locations;
+        for (let i = 0; i < this.numShips; i++) {
+            do {
+                locations = this.generateShip();
+            } while (this.collision(locations));
+            this.ships[i].locations = locations;
+        }
+    },
+    generateShip: function () {
+        let direction = Math.floor(Math.random() * 2);
+        let row, col;
+        if (direction === 1) {
+            row = Math.floor(Math.random() * this.boardSize);
+            col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+        } else {
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+            col = Math.floor(Math.random() * this.boardSize);
+        }
+
+
+        let newShipLocations = [];
+        for (let i = 0; i < this.shipLength; i++) {
+            if (direction === 1) {
+                newShipLocations.push(row + '' + (col + i));
+            } else {
+                newShipLocations.push((row + i) + '' + col);
+            }
+
+        }
+        return newShipLocations;
+    },
+    collision: function (locations) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = model.ships[i];
+            for (let j = 0; j < locations.length; j++) {
+                if (ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
+
